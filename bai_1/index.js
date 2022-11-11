1f446ce516b6cceaa407cd589c677a6e9d53a82f
NguyenPhuc123789 <cute.hotelmanage@gmail.com>
2022-11-11 08:59:39 -0500

    AON

---
+++ bai_1/index.js
@@ -0,0 +1,25 @@
+// kiểu 1
+function myname(){
+    console.log("NGuyễn PHÚc")
+}
+///kiểu 2
+function myname1(){
+    console.log()
+
+
+}
+myname1()
+
+
+
+
+
+/// hình thnag
+function dientichhinhthnag(dayLon, dayBe, chieuCao){
+    return (dayLon + dayBe )/2 + chieuCao
+
+      
+}
+
+
+console.log(dientichhinhthnag(2, 3, 3));
